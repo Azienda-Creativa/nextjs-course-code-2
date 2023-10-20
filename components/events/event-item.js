779f6c333a5102d46ee404aee0ee486aa04c6f6b
Azienda-Creativa <116496155+Azienda-Creativa@users.noreply.github.com>
2023-10-20 15:32:14 +0200

    progress

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,7 @@
-import Link from "next/link"
+import Button from "../ui/button"
+import DateIcon from "../icons/date-icon"
+import AddressIcon from "../icons/address-icon"
+import ArrowRightIcon from "../icons/arrow-right-icon"
 import classes from "./event-item.module.css"
 
 export default function EventItem(props) {
@@ -10,6 +13,7 @@ export default function EventItem(props) {
   })
 
   const formattedAddress = location.replace(", ", "\n")
+  const seemore = `/events/${id}`
 
   return (
     <li className={classes.item}>
@@ -21,14 +25,21 @@ export default function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{formattedDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={`/events/${id}`}>Link to event</Link>
+          <Button link={seemore}>
+            <span>Explore events</span>{" "}
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,12 @@
+import { useRouter } from "next/router"
+import { getEventById } from "../../data"
 export default function EventDetailPage() {
+  const router = useRouter()
+  const id = router.query.eventId
+  const event = getEventById(id)
+
+  !event && <h3>Not event found!</h3>
+
   return (
     <div>
       <h1>Single Event Page</h1>
